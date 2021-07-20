61f2ecafb739987a3d637ca4989e33fb4c92ba6c
Mischback <dev@mischback.de>
2021-07-20 12:38:05 +0200

    Dead End

--- _util/bin/hashed_assets_manifest.js
+++ _util/bin/hashed_assets_manifest.js
@@ -1,11 +1,41 @@
 
 const dirTree = require("directory-tree");
 const _path = require("path");
+const crypto = require("crypto");
+const fs = require("fs");
 
-const tree = dirTree(
-  "build",
-  {},
-  (item, _path, stats ) => {
-    console.log(item);
-  }
-);
+function fileHash(file) {
+  console.log("[fileHash] " + "foo");
+  return new Promise((resolve, reject) => {
+    const hash = crypto.createHash("md5");
+    const stream = fs.createReadStream(file);
+    stream.on("error", err => reject(err));
+    stream.on("data", chunk => hash.update(chunk));
+    stream.on("end", () => resolve(hash.digest("hex")));
+  });
+}
+
+function typeFileCallback(item, _path, stats) {
+  console.log("[typeFileCallback] " + item.path);
+  fileHash(item.path).then((hash) => {
+    console.log("[typeFileCallback] hash computation completed!")
+    return hash;
+  });
+}
+
+
+const main = function() {
+  console.log("[start]");
+
+  var result_tree = dirTree(
+    "build",
+    {},
+    typeFileCallback
+  );
+
+  console.log(result_tree);
+
+  console.log("[finished]");
+};
+
+main();
