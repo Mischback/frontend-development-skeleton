4df421a9b1756dc78344fe98c1f5bfaccbf820c2
Mischback <dev@mischback.de>
2021-07-20 16:38:04 +0200

    Basically working, including renaming

--- _util/bin/hashed_assets_manifest.js
+++ _util/bin/hashed_assets_manifest.js
@@ -3,43 +3,58 @@ const fs = require("fs");
 const path = require("path");
 const crypto = require("crypto");
 
+/* Recursive asynchronous directory walker
+ *
+ * Works with Promises
+ *  - resolves to a list of files
+ *  - rejects with an error message
+ */
 function dirWalk(dir) {
   var results = [];
 
-  return new Promise(function(resolve, reject) {
-    fs.readdir(dir, function(err, list) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(dir, (err, list) => {
       if (err) reject(err);
 
       var pending = list.length;
       if (!pending) resolve(results);
 
-      list.forEach(function(file) {
+      list.forEach(file => {
         file = path.resolve(dir, file);
-        fs.stat(file, function(err, stat) {
+        fs.stat(file, (err, stat) => {
+          // sub-directories are handled with a recursive call
           if (stat && stat.isDirectory()) {
             dirWalk(file).then(
-              (res) => {
+              // resolve handling (fuse results)
+              res => {
                 results = results.concat(res);
                 if (!--pending) resolve(results);
               },
-              (err) => {
-                reject(err);
-              }
+              // reject handling (reject upwards)
+              err => reject(err)
             );
           } else {
-            fileHash(file).then(
-              function(hash) {
-                console.log("[dirWalk] hash created for " + file + "!");
-                results.push({
-                  "path": file,
-                  "hash": hash
-                });
-                if (!--pending) resolve(results);
-              },
-              function(err) {
-                reject(err);
-              }
-            )
+            // handling of files! Here be magic!
+            fileFilter(file, "css")
+              .then(
+                fileHash,
+                err => reject(err)
+              )
+              .then(
+                hash => renameFile(file, hash),
+                err => reject(err)
+              )
+              .then(
+                newFileName => {
+                  console.log(file + " -> " + newFileName);
+                  results.push({
+                    "file": file,
+                    "hashed": newFileName
+                  });
+                  if (!--pending) resolve(results);
+                },
+                err => reject(err)
+              )
           }
         });
       });
@@ -47,9 +62,26 @@ function dirWalk(dir) {
   });
 }
 
+function renameFile(file, hash) {
+  // console.log("[renameFile]: " + file + ": " + hash);
+  return new Promise((resolve, reject) => {
+    var filePath = path.dirname(file);
+    var fileExt = path.extname(file);
+    var fileBaseName = path.basename(file, fileExt);
+
+    // TODO: possible limit the length of the hash here!
+    var newFileName = path.join(filePath, fileBaseName + "." + hash + fileExt);
+    // console.log("[renameFile] new name: " + newFileName);
+
+    // actually rename the file on disk!
+    fs.rename(file, newFileName, err => reject("Could not rename file!"));
+    resolve(newFileName);
+  });
+}
+
 
 function fileHash(file) {
-  return new Promise(function(resolve, reject) {
+  return new Promise((resolve, reject) => {
     // reject("foo");
     const hash = crypto.createHash("md5");
     const stream = fs.createReadStream(file);
@@ -59,15 +91,29 @@ function fileHash(file) {
   });
 }
 
+function fileFilter(file, extensions) {
+  return new Promise((resolve, reject) => {
+    var fileExt = file.slice((file.lastIndexOf(".") - 1 >>> 0) + 2);
+    // console.log("[fileFilter] found: " + fileExt);
+
+    // TODO: Here the file extension filter must be implemented!
+    if (fileExt === extensions) resolve(file);
+
+    reject("Does not match extension!");
+  });
+}
+
 
 function main() {
   dirWalk("build").then(
     (result) => {
       console.log("dirWalk finished");
       console.log(result);
+      return 0;
     },
     (err) => {
       console.error("something went wrong!");
+      return -1;
     })
 };
 
