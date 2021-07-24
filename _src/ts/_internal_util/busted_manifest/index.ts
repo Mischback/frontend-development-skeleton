573d871f9e7663088bc2ba4d31d75932efe38587
Mischback <dev@mischback.de>
2021-07-24 20:52:08 +0200

    Promisified fs.readdir()

--- _src/ts/_internal_util/busted_manifest/index.ts
+++ _src/ts/_internal_util/busted_manifest/index.ts
@@ -3,6 +3,7 @@ import fs = require("fs");
 import path = require("path");
 import util = require("util");
 
+const fsreaddir = util.promisify(fs.readdir);
 const fsstat = util.promisify(fs.stat);
 
 /* project files */
@@ -37,65 +38,68 @@ function hashWalker(dir: string, extensions: string[]): Promise<string[]> {
   let results: string[] = [];
 
   return new Promise((resolve, reject) => {
-    fs.readdir(dir, (err, list) => {
-      if (err)
-        return reject(
-          new BustedManifestFileSystemError("Error while reading directory")
-        );
+    fsreaddir(dir)
+      .then((list) => {
+        /* Check if there are still things to do. If nothing is pending, resolve
+         * with the overall results.
+         */
+        let pending = list.length;
+        if (!pending) return resolve(results);
 
-      /* Check if there are still things to do. If nothing is pending, resolve
-       * with the overall results.
-       */
-      let pending = list.length;
-      if (!pending) return resolve(results);
+        /* at this point, there are still items to be iterated in list */
+        list.forEach((file) => {
+          /* Make the file path absolute */
+          file = path.resolve(dir, file);
 
-      /* at this point, there are still items to be iterated in list */
-      list.forEach((file) => {
-        /* Make the file path absolute */
-        file = path.resolve(dir, file);
-
-        fsstat(file)
-          .then((stat) => {
-            if (stat.isDirectory()) {
-              /* handle sub-directories with recursive call */
-              hashWalker(file, extensions).then(
-                /* recursive call succeeded, merge the results */
-                (result) => {
-                  results = results.concat(result);
-                  if (!--pending) return resolve(results);
-                },
-                /* the recursive call failed, just pass the original error upwards */
-                (err) => reject(err)
-              );
-            } else {
-              /* handle files:
-               *   - match against the provided list of extensions
-               *   - actually hash the file's content
-               *   - rename / copy the file
-               *   - create record for the manifest file
-               */
-              filterByExtension(file, extensions)
-                .then((file) => {
-                  results.push(file);
-                })
-                .catch((err) => {
-                  if (!(err instanceof BustedManifestFilterByExtensionMismatch))
-                    return reject(err);
-                })
-                .finally(() => {
-                  if (!--pending) return resolve(results);
-                });
-            }
-          })
-          .catch(() =>
-            reject(
-              new BustedManifestFileSystemError(
-                "Error while accessing file stat"
+          fsstat(file)
+            .then((stat) => {
+              if (stat.isDirectory()) {
+                /* handle sub-directories with recursive call */
+                hashWalker(file, extensions).then(
+                  /* recursive call succeeded, merge the results */
+                  (result) => {
+                    results = results.concat(result);
+                    if (!--pending) return resolve(results);
+                  },
+                  /* the recursive call failed, just pass the original error upwards */
+                  (err) => reject(err)
+                );
+              } else {
+                /* handle files:
+                 *   - match against the provided list of extensions
+                 *   - actually hash the file's content
+                 *   - rename / copy the file
+                 *   - create record for the manifest file
+                 */
+                filterByExtension(file, extensions)
+                  .then((file) => {
+                    results.push(file);
+                  })
+                  .catch((err) => {
+                    if (
+                      !(err instanceof BustedManifestFilterByExtensionMismatch)
+                    )
+                      return reject(err);
+                  })
+                  .finally(() => {
+                    if (!--pending) return resolve(results);
+                  });
+              }
+            })
+            .catch(() =>
+              reject(
+                new BustedManifestFileSystemError(
+                  "Error while accessing file stat"
+                )
               )
-            )
-          );
-      });
-    });
+            );
+        });
+      })
+      .catch(() =>
+        reject(
+          new BustedManifestFileSystemError("Error while reading directory")
+        )
+      );
   });
 }
 
